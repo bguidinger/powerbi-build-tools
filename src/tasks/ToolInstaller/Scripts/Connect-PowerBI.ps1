62fbb156efff56aa6749b55f48e126663936e954
Bob Guidinger <bguidinger@gmail.com>
2020-03-11 06:22:40 -0500

    Added satisfies/demands to tasks (resolves #6)

--- src/tasks/ToolInstaller/Scripts/Connect-PowerBI.ps1
+++ src/tasks/ToolInstaller/Scripts/Connect-PowerBI.ps1
@@ -2,38 +2,30 @@
 
 param()
 
-Trace-VstsEnteringInvocation $MyInvocation
+$toolsPath = Get-VstsTaskVariable -Name "PowerBI_Tools_Path"
 
-try
+Import-Module -Name "$toolsPath/Modules/MicrosoftPowerBIMgmt.Profile"
+
+# Connection
+$connection = Get-VstsInput -Name Connection
+$endpoint = Get-VstsEndpoint -Name $connection
+$authScheme = $endpoint.Auth.Scheme
+$environment = $endpoint.Data.Environment
+
+If ($authScheme -eq "UsernamePassword")
 {
-	$toolsPath = Get-VstsTaskVariable -Name "PowerBI_Tools_Path"
-	Import-Module -Name "$toolsPath/Modules/MicrosoftPowerBIMgmt.Profile"
+	$username = $endpoint.Auth.Parameters.Username
+	$password = ConvertTo-SecureString $endpoint.Auth.Parameters.Password -AsPlainText -Force
+	$credential = New-Object System.Management.Automation.PSCredential $username, $password
 
-	# Connection
-	$connection       = Get-VstsInput -Name Connection
-	$endpoint         = Get-VstsEndpoint -Name $connection
-	$authScheme       = $endpoint.Auth.Scheme
-	$environment      = $endpoint.Data.Environment
-
-	If ($authScheme -eq "UsernamePassword")
-	{
-		$username     = $endpoint.Auth.Parameters.Username
-		$password     = ConvertTo-SecureString $endpoint.Auth.Parameters.Password -AsPlainText -Force
-		$credential   = New-Object System.Management.Automation.PSCredential $username, $password
-
-		Connect-PowerBIServiceAccount -Environment $environment -Credential $credential
-	}
-	Else
-	{
-		$tenantId     = $endpoint.Auth.Parameters.TenantId	
-		$clientId     = $endpoint.Auth.Parameters.ClientId
-		$clientSecret = ConvertTo-SecureString $endpoint.Auth.Parameters.ClientSecret -AsPlainText -Force
-		$credential   = New-Object System.Management.Automation.PSCredential $clientId, $clientSecret
-
-		Connect-PowerBIServiceAccount -Environment $environment -Tenant $tenantId -Credential $credential -ServicePrincipal
-	}
+	Connect-PowerBIServiceAccount -Environment $environment -Credential $credential
 }
-finally
+Else
 {
-	Trace-VstsLeavingInvocation $MyInvocation
+	$tenantId = $endpoint.Auth.Parameters.TenantId	
+	$clientId = $endpoint.Auth.Parameters.ClientId
+	$clientSecret = ConvertTo-SecureString $endpoint.Auth.Parameters.ClientSecret -AsPlainText -Force
+	$credential = New-Object System.Management.Automation.PSCredential $clientId, $clientSecret
+
+	Connect-PowerBIServiceAccount -Environment $environment -Tenant $tenantId -Credential $credential -ServicePrincipal
 }
