4ac22ad926db9a7dab3c7c4a107c671caa1f5fc8
Bob Guidinger <bguidinger@gmail.com>
2020-03-11 12:03:37 -0500

    Fixed bug in tool installer

--- src/tasks/ToolInstaller/Run.ps1
+++ src/tasks/ToolInstaller/Run.ps1
@@ -9,7 +9,7 @@ if (-not (Test-Path $toolsPath))
 	New-Item -ItemType Directory $toolsPath | Out-Null
 }
 
-Move-Item Modules $toolsPath -Force
-Move-Item Scripts $toolsPath -Force
+Copy-Item Modules $toolsPath -Recurse -Force
+Copy-Item Scripts $toolsPath -Recurse -Force
 
 Set-VstsTaskVariable -Name "PowerBI_Tools_Path" -Value $toolsPath
